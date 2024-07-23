68d599e9fb005b80cb39e439472b4dc2ecf087a6
piyush2712shah <164843354+piyush2712shah@users.noreply.github.com>
2024-07-23 10:05:22 +0545

    Update SocketContext.jsx

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("http://localhost:5500", {
+			const socket = io("https://talk-a-tive-375w.onrender.com", {
 				query: {
 					userId: authUser._id,
 				},
